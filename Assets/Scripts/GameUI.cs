5dd76dac7daf8dd4dcb508e9956041457585195e
GrafEnters <graf.enters@gmail.com>
2024-05-02 23:10:05 +0300

    UI almost done

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -1,5 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class GameUI : MonoBehaviour { }
+public class GameUI : MonoBehaviour {
+    [SerializeField]
+    private LeaderboardDialog _leaderboardDialog;
+
+    [SerializeField]
+    private ARView _arView;
+
+    [SerializeField]
+    private PlayerHpView _playerHpView;
+
+    [SerializeField]
+    private AmmoView _ammoView;
+
+    [SerializeField]
+    private DeathmatchProgressView _deathmatchProgressView;
+
+    public static GameUI Instance;
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+            _leaderboardDialog.gameObject.SetActive(true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Tab)) {
+            _leaderboardDialog.gameObject.SetActive(false);
+        }
+    }
+}
