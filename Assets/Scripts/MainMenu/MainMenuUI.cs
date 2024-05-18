a5903742249db010ce87f8c3902d6d3df48ef4a7
GrafEnters <graf.enters@gmail.com>
2024-05-18 19:55:48 +0300

    Added shipUpgradeDialog

--- Assets/Scripts/MainMenu/MainMenuUI.cs
+++ Assets/Scripts/MainMenu/MainMenuUI.cs
@@ -1,5 +1,10 @@
 using UnityEngine;
 
 public class MainMenuUI : MonoBehaviour {
-   
+    [SerializeField]
+    private ShipUpgradeDialog _shipUpgradeDialog;
+
+    public void ToggleShipUpgradeDialog() {
+        _shipUpgradeDialog.Open();
+    }
 }
